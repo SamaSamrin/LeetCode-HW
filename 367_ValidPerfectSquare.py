d7202f1d3604b655d03616535f5ad96161a469f2
Sama Samrin Orpa <samasamrin@gmail.com>
2022-01-04 00:39:01 +0600

    Create 367_ValidPerfectSquare.py

---
+++ 367_ValidPerfectSquare.py
@@ -0,0 +1,14 @@
+import math
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        if (math.sqrt(num)).is_integer():
+            return True
+        else:
+            return False
+
+          
+"""
+  Runtime: 20 ms
+  Memory Usage: 13.2 MB
+"""
